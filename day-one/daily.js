b3b7f5e4dbea31f0e99f9de823fd317fdb8bdf3c
David Vloedman <djvloedman@gmail.com>
2020-01-13 09:26:31 -0800

    Filter

--- day-one/daily.js
+++ day-one/daily.js
@@ -1,16 +1,26 @@
-'use strict';
+'use strict'
 
-const map = (arr, pnter) => {
+const myArr = [1, 2, 3, 4, 5];
+
+const map = (arr, pntr) => {
   const mapped = [];
-  arr.forEach(itm => mapped.push(pnter(itm)));
+  arr.forEach(itm => mapped.push(pntr(itm)));
   return mapped;
 };
 
-const reduce = (arr, pnter) => {
+const reduce = (arr, pntr) => {
   let accumulator = 0;
   arr.forEach(itm => {
-    accumulator = pnter(accumulator, itm);    
+    accumulator = pntr(accumulator, itm);
+    console.log(accumulator);
   });
   return accumulator;
 };
 
+const filter = (arr, pntr) => {
+  const filtered = [];
+  arr.forEach(itm => {
+    if (pntr(itm)) filtered.push(itm);
+  });
+  return filtered;
+};
